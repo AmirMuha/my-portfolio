9eb5aa3d8266ce680b4ad9e071ca631b9c281023
AmirMuha <mirzaii4046@gmail.com>
2021-09-23 23:35:03 +0400

    add: new constants added - new environment varaibles

--- server/src/constants/environment-variables.ts
+++ server/src/constants/environment-variables.ts
@@ -1,3 +1,7 @@
 export const PORT = process.env.PORT || 3333;
 export const HOST = process.env.HOST || "localhost";
 export const __prod__ = process.env.NODE_ENV === "production";
+export const GRAPHQL_API: string = process.env.GRAPHQL_API_TOKEN!;
+export const KEY_GENERATOR_SECRET: string = process.env.KEY_GENERATOR_SECRET!;
+export const SESSION_SECRET: string = process.env.SESSION_SECRET!;
+export const SESSION_MAX_AGE: number = parseInt(process.env.SESSION_MAX_AGE!);
