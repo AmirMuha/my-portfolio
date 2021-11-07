e4be760c92051e1d454d1fca5e502e81d4d1f055
amirmuha <mirzaii4046@gmail.com>
2021-11-08 00:29:13 +0400

    fix: refetching projects and unsavedProjects

--- client/src/components/Dashboard/templates/Dashboard.tsx
+++ client/src/components/Dashboard/templates/Dashboard.tsx
@@ -1,25 +1,35 @@
-import { useQuery } from "@apollo/client"
-import React, { FC } from "react"
 import {
   QueryMessages,
   QueryProjectsWithTechCategories,
 } from "../../../util/queries"
-import Stack from "../../App/Stack"
-import TheSection from "../../App/TheSection"
-import { SEO } from "../../SEO"
-import Loading from "../../UI/Loading"
+import React, { FC, useEffect } from "react"
+
 import Dash_Hero from "../Dash_Hero"
 import Dash_Layout from "../Dash_Layout"
 import Dash_Message from "../Dash_Message"
 import Dash_Projects from "../Dash_Projects"
+import Loading from "../../UI/Loading"
+import { SEO } from "../../SEO"
+import Stack from "../../App/Stack"
+import TheSection from "../../App/TheSection"
+import UnsavedProjects from "../UnsavedProjects"
+import { useQuery } from "@apollo/client"
 
 const Dashboard: FC = () => {
-  const { data: projectQueryData, loading: projectsLoading } = useQuery<{
+  const {
+    data: projectQueryData,
+    loading: projectsLoading,
+    refetch: refetchProjects,
+  } = useQuery<{
     projects: GatsbyTypes.Portfolio_Project[]
   }>(QueryProjectsWithTechCategories)
   const { data: messagesQueryData, loading: messagesLoading } = useQuery<{
     messages: GatsbyTypes.Portfolio_Message[]
   }>(QueryMessages)
+  useEffect(() => {
+    refetchProjects()
+  }, [])
+
   if (projectsLoading || messagesLoading) {
     return <Loading />
   }
@@ -27,6 +37,7 @@ const Dashboard: FC = () => {
     <>
       <SEO title="Dashboard" />
       <Dash_Layout>
+        <UnsavedProjects />
         <div className="my-10">
           <Dash_Hero />
           <div className="">
