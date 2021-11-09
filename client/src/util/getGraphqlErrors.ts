9dbde0cfd8f98f43c2692830cfafbd8944dca6d1
amirmuha <mirzaii4046@gmail.com>
2021-11-09 23:38:27 +0330

    fix: error handled if validationErrors is undefined

--- client/src/util/getGraphqlErrors.ts
+++ client/src/util/getGraphqlErrors.ts
@@ -4,15 +4,19 @@ export const getGraphqlErrors = (obj: any): string[] => {
   }
   const allErrors: string[] = []
   for(const err of obj[obj.errors ? "errors" : "graphQLErrors"]) {
-    const children: any[] =
-      err.extensions.exception
-        .validationErrors[0].children
-    for (const child of children) {
-      const property = child.property
-      const constraints = child.constraints
-      for (const con in constraints) {
-        allErrors.push(`Error in ${property}: ${constraints[con]}`)
+    if(err.extensions.exception.validationErrors) {
+      const children: any[] =
+        err.extensions.exception
+          .validationErrors[0].children
+      for (const child of children) {
+        const property = child.property
+        const constraints = child.constraints
+        for (const con in constraints) {
+          allErrors.push(`Error in ${property}: ${constraints[con]}`)
+        }
       }
+    } else {
+      allErrors.push(err.message || "Unknown Error: Something went wrong, try again later.")
     }
   }
   return allErrors
