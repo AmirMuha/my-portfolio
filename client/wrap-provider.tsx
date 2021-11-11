15065df638b2c3f4c247a70014721e048c966afe
amirmuha <mirzaii4046@gmail.com>
2021-11-11 13:46:57 +0330

    fix: subscription working with websocket fixed

--- client/wrap-provider.tsx
+++ client/wrap-provider.tsx
@@ -7,35 +7,37 @@ import {
 import React, { FC } from "react"
 
 import { Provider } from "react-redux"
-import { WebSocketLink } from '@apollo/client/link/ws';
+import { WebSocketLink } from "@apollo/client/link/ws"
 import { WrapRootElementBrowserArgs } from "gatsby"
 import { createUploadLink } from "apollo-upload-client"
 import fetch from "cross-fetch"
-import {getMainDefinition} from "@apollo/client/utilities"
+import { getMainDefinition } from "@apollo/client/utilities"
 import { store } from "./src/store/store"
 
-const wsLink = new WebSocketLink({
-  uri: 'ws://localhost:3333/graphql',
+const wsLink = window ? new WebSocketLink({
+  uri: "ws://localhost:3333/graphql",
   options: {
-    reconnect: true
-  }
-});
+    reconnect: true,
+  },
+}): null
+
 const uploadLink = createUploadLink({
   credentials: "include",
   uri: "http://localhost:3333/graphql",
   fetch,
 })
-const splitLink = split(
+
+const splitLink = window ? split(
   ({ query }) => {
-    const definition = getMainDefinition(query);
+    const definition = getMainDefinition(query)
     return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    );
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    )
   },
-  wsLink,
-  uploadLink,
-);
+  wsLink as any,
+  uploadLink
+): uploadLink
 export const wrapRootElement: FC<WrapRootElementBrowserArgs> = ({
   element,
 }) => {
@@ -44,7 +46,7 @@ export const wrapRootElement: FC<WrapRootElementBrowserArgs> = ({
   }
   const cache = new InMemoryCache()
   const client = new ApolloClient({
-    link: splitLink,
+    link: splitLink || uploadLink,
     connectToDevTools: true,
     cache,
   })
