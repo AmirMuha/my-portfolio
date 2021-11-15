3c7ffd694b0323365cd81bd7a5d0d308667af219
amirmuha <mirzaii4046@gmail.com>
2021-11-15 20:27:17 +0330

    fix: removed confirmPassword field from reseting password and handling it on frontend

--- server/src/types/arg-types/ResetPasswordArgs.ts
+++ server/src/types/arg-types/ResetPasswordArgs.ts
@@ -1,10 +1,9 @@
-import { IsEmail, IsInt, IsString, Length, MinLength } from "class-validator";
 import { ArgsType, Field, Int } from "type-graphql";
+import { IsEmail, IsInt, IsString, Length, MinLength } from "class-validator";
 
 export interface RPArgs {
   email: string;
   newPassword: string;
-  confirmPassword: string;
   token?: string;
   code?: number;
 }
@@ -19,11 +18,6 @@ export class ResetPasswordArgs implements RPArgs {
   @MinLength(8)
   @Field(() => String)
   newPassword: string;
-
-  @IsString()
-  @MinLength(8)
-  @Field(() => String)
-  confirmPassword: string;
 
   @IsString()
   @Field(() => String, { nullable: true })
