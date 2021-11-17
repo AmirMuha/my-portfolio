b62f7015beb2114fa3a1b76660376b61d6cbee0a
amirmuha <mirzaii4046@gmail.com>
2021-11-17 20:20:37 +0330

    fix: configured path from .env files only in development environment

--- server/src/utils/configuringEnvironmentVariables.ts
+++ server/src/utils/configuringEnvironmentVariables.ts
@@ -1,4 +1,8 @@
-import { config as EnvConfig } from "dotenv";
-EnvConfig({
-  path: `.env.${process.env.NODE_ENV}`,
-});
+import {__prod__} from "../constants/envs-and-consts"
+if(!__prod__) {
+  import("dotenv").then((EnvConfig) => {
+    EnvConfig.config({
+      path: `.env.${process.env.NODE_ENV}`,
+    });
+  })
+}
