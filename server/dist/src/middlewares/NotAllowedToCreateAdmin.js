915b9f41c379b8fdeeb9dce62f07089ef4abd2f6
amirmuha <mirzaii4046@gmail.com>
2021-12-08 18:51:08 +0330

    not tracking dist folder anymore

--- server/dist/src/middlewares/NotAllowedToCreateAdmin.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NotAllowedToCreateAdmin = void 0;
-const NotAllowedToCreateAdmin = async () => {
-    throw new Error("You cannot create a new admin, there is on already.");
-};
-exports.NotAllowedToCreateAdmin = NotAllowedToCreateAdmin;
-//# sourceMappingURL=NotAllowedToCreateAdmin.js.map
